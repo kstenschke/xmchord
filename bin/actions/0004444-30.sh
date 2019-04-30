9d1ca9f3637ff7daf651952f157edbbb766e2cda
kay.stenschke <info@stenschke.com>
2019-04-30 11:45:27 +0200

    added actions: bl+a,c,v,x,y,z

--- bin/actions/0004444-30.sh
+++ bin/actions/0004444-30.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-#: TL + U - Chromium: focus URL, Evolution: activate "unread" filter
+#: BL + A - Global: Fire CTRL+A / Evolution: switch to "all messages" filter
 
 focusApplication=`cat /proc/$(xdotool getwindowpid $(xdotool getwindowfocus))/comm`
-if [[ "$focusApplication" =~ "chromium-browse" ]] || [[ "$focusApplication" =~ "firefox" ]]; then
-    xdotool sleep 0.1
-    xdotool key Ctrl+l
-    exit 0;
-fi
 
 if [[ "$focusApplication" =~ "evolution" ]]; then
+    # Evolution has focus: Activate "All Messages" filter
     xdotool sleep 0.1
+
     xdotool keydown alt key Left
     xdotool key w
     xdotool keyup alt
     xdotool sleep 0.1
     xdotool key space
     xdotool sleep 0.1
-    xdotool key Down
+    xdotool key Home
     xdotool key space
+
+    exit 0;
 fi
+
+echo 'select all'
+xdotool sleep 0.1
+xdotool key ctrl+a
--- bin/actions/1010101-30.sh
+++ bin/actions/1010101-30.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-#: BL + A - Global: Fire CTRL+A / Evolution: switch to "all messages" filter
+#: BL + A - Select all (Fire CTRL+A)
 
-focusApplication=`cat /proc/$(xdotool getwindowpid $(xdotool getwindowfocus))/comm`
-
-if [[ "$focusApplication" =~ "evolution" ]]; then
-    # Evolution has focus: Activate "All Messages" filter
-    xdotool sleep 0.1
-
-    xdotool keydown alt key Left
-    xdotool key w
-    xdotool keyup alt
-    xdotool sleep 0.1
-    xdotool key space
-    xdotool sleep 0.1
-    xdotool key Home
-    xdotool key space
-
-    exit 0;
-fi
-
-echo 'select all'
 xdotool sleep 0.1
 xdotool key ctrl+a
