69b3d000fa02ac8b9b514f50b0f6d3a42cfe3c5a
Kay Stenschke <info@stenschke.com>
2019-05-02 13:16:26 +0200

    Add global "find" hotkey (BL+f)

--- bin/actions/1010101-33.sh
+++ bin/actions/1010101-33.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-#: BL + F - If terminal has focus: Open search popup 
+#: BL + F - Global / if terminal has focus: Open search popup
 
 focusApplication=`cat /proc/$(xdotool getwindowpid $(xdotool getwindowfocus))/comm`
 if [[ "$focusApplication" =~ "gnome-terminal-" ]]; then
     # gnome terminal has focus
     xdotool key Control_L+Shift+f
+    exit 0;
 fi
+
+xdotool sleep 0.1
+xdotool key ctrl+f
