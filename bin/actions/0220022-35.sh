d135922d70ec67b048dae5d38fb2d0422f9892f2
kstenschke <info@stenschke>
2021-01-11 08:59:50 +0100

    restore claws mail action: toggle "hide read"

--- bin/actions/0220022-35.sh
+++ bin/actions/0220022-35.sh
@@ -5,30 +5,50 @@
 
 focusApplication=$(cat /proc/$(xdotool getwindowpid $(xdotool getwindowfocus))/comm)
 
-if [[ "$focusApplication" =~ "gnome-terminal-" ]]; then
+if [[ "$focusApplication" =~ "claws-mail" ]]; then
+  echo "Toggle hide read threads" | aosd_cat -R white -u 300 --y-offset=40 \
+  -B black -b 240 --padding=24 -x 5 -n "Ubuntu 20" -p 1
+
+  xdotool key Alt+v  # Expand "View" menu
+
+  for i in {1..8}  # Select "hide read threads" option
+  do
+    xdotool key Down
+  done
+
+  xdotool key space  # Toggle selected option
   xdotool sleep 0.1
-  xdotool key Control_L+d
+
+  xdotool key Escape  # Close menu
+  xdotool sleep 0.1
+
+  xdotool key Return  # View 1st unread thread
 else
-  if [[ "$focusApplication" =~ "chromium-browse" ]] ||
-    [[ "$focusApplication" =~ "firefox" ]]; then
+  if [[ "$focusApplication" =~ "gnome-terminal-" ]]; then
     xdotool sleep 0.1
-    xdotool key Ctrl+l
-    xdotool sleep 0.1
-    xdotool key Ctrl+a
-    xdotool key Ctrl+c
-    xdotool key Home
+    xdotool key Control_L+d
+  else
+    if [[ "$focusApplication" =~ "chromium-browse" ]] ||
+      [[ "$focusApplication" =~ "firefox" ]]; then
+      xdotool sleep 0.1
+      xdotool key Ctrl+l
+      xdotool sleep 0.1
+      xdotool key Ctrl+a
+      xdotool key Ctrl+c
+      xdotool key Home
 
-    clipboard=`xsel -ob`
+      clipboard=`xsel -ob`
 
-    if [[ $clipboard == view-source* ]]; then
-      for i in {1..12}; do
-        xdotool key Delete
-      done
-    else
-      xdotool type "view-source:"
-      xdotool key End
+      if [[ $clipboard == view-source* ]]; then
+        for i in {1..12}; do
+          xdotool key Delete
+        done
+      else
+        xdotool type "view-source:"
+        xdotool key End
+      fi
+
+      xdotool key Return
     fi
-
-    xdotool key Return
   fi
 fi
