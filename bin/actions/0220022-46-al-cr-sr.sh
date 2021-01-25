9edcb36b1b3cb2ee4e36f479bbefaa100df5f1b2
kstenschke <info@stenschke>
2021-01-25 23:23:42 +0100

    add current value to output of volume up/down actions

---
+++ bin/actions/0220022-46-al-cr-sr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# workaround while "◢ + C" emits event code w/ trailing modifiers "al-cr-sr"
+
+path_self="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+"$path_self"/0220022-46.sh
+unset path_self
--- bin/actions/1010101-108.sh
+++ bin/actions/1010101-108.sh
@@ -4,15 +4,17 @@
 
 amixer -D pulse sset Master 5%- &> /dev/null
 
-#currentVolume=`amixer -D pulse sget Master | \
-#grep 'Front Left: Playback' | \
-#grep -oE "[0-9]{1,3}%"`
-#notify-send "Volume lower ($currentVolume)" -t 1
+currentVolume=$(amixer -D pulse sget Master | \
+grep "Front Left: Playback" | \
+grep -oE "[0-9]{1,3}%")
 
-echo "Volume--" | aosd_cat -R white -u 300 --y-offset=40 -B black -b 240 \
+echo "Volume-- ($currentVolume)" | \
+aosd_cat -R white -u 300 --y-offset=40 -B black -b 240 \
 --padding=24 -x 5 -n "Ubuntu 20" -p 1
 
 xdotool key Up
 
 sleep 0.1
 xdotool key Escape  # hide context menu popup
+
+unset currentVolume
