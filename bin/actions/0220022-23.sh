ab4576552b80edf908fcea3b9dd644efdef53929
kstenschke <info@stenschke>
2021-01-27 10:00:45 +0100

    cleanup

--- bin/actions/0220022-23.sh
+++ bin/actions/0220022-23.sh
@@ -16,7 +16,6 @@ wifi_mac=$(ip addr show \
 "$(awk 'NR==3{print $1}' /proc/net/wireless | tr -d :)" \
 | awk '/ether/{print $2}')
 
-
 echo "$public_ip" | xclip -in -selection clipboard
 
 (sleep 0.1; printf "Public IP: %s (Copied)\rPrivate IP: %s\rWifi MAC: %s" \
