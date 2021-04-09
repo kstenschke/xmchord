cac79715ac0e9442e6417499a39f0e0e7a4c89cc
kstenschke <info@stenschke.com>
2021-04-09 16:31:43 +0200

    remove unwanted input before launching xshmenu

--- bin/actions/0220022-78.sh
+++ bin/actions/0220022-78.sh
@@ -2,4 +2,10 @@
 
 #: ◣ + numeric [+] - Open xshmenu
 
+focusApplication=\
+$(cat /proc/"$(xdotool getwindowpid "$(xdotool getwindowfocus)")"/comm)
+
+path_self="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+"$path_self"/utils/remove_unwanted_output.sh "$focusApplication"
+
 xshmenu -pp -o=nw
