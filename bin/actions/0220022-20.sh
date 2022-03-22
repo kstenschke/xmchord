4de56290e13182cc46ddc67316ef9f323d0e0695
kay <info@stenschke.com>
2022-03-22 08:18:07 +0100

    Delay toggle always-on-top notification a bit / make it more reliable

--- bin/actions/0220022-20.sh
+++ bin/actions/0220022-20.sh
@@ -10,17 +10,19 @@ path_self="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
 wmctrl -r :ACTIVE: -b toggle,above
 
+xdotool sleep 0.2
+
 activeWindowId=$(xdotool getactivewindow)
 activeWindowProps=$(xprop -id "${activeWindowId}" | grep _NET_WM_STATE)
 
 if [[ ${activeWindowProps} == *"_NET_WM_STATE_ABOVE"* ]]; then
-#    notify-send "Always on top: On" -t 1
-    echo "Always on top" | aosd_cat -R white -u 300 --y-offset=40 -B green \
-    -b 240 --padding=24 -x 5 -n "Ubuntu 20" -p 1
+  # notify-send "Always on top: On" -t 1
+  echo "Always on top" | aosd_cat -R white -u 300 --y-offset=40 -B green \
+  -b 240 --padding=24 -x 5 -n "Ubuntu 20" -p 1
 else
-#    notify-send "Always on top: Off" -t 1
-    echo "Always on top" | aosd_cat -R white -u 300 --y-offset=40 -B red \
-    -b 240 --padding=24 -x 5 -n "Ubuntu 20" -p 1
+  # notify-send "Always on top: Off" -t 1
+  echo "Always on top" | aosd_cat -R white -u 300 --y-offset=40 -B red \
+  -b 240 --padding=24 -x 5 -n "Ubuntu 20" -p 1
 fi;
 
 unset focusApplication
